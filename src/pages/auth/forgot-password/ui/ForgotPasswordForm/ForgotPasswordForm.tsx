52d3d910a610eadbac7106b450a30048fc31eb50
Лёша <airalex04@MacBook-Air-Lesa.local>
2026-08-27 15:40:37 +0300

    forgot pass form + recoverypasssent

--- src/pages/auth/forgot-password/ui/ForgotPasswordForm/ForgotPasswordForm.tsx
+++ src/pages/auth/forgot-password/ui/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface ForgotPasswordFormData {
@@ -5,6 +6,9 @@ interface ForgotPasswordFormData {
 }
 
 export function ForgotPasswordForm() {
+
+    const [isRecoveyPassEmailSent, setRecoveyPassEmailSent] = useState(false);
+
     const {
         register,
         handleSubmit,
@@ -13,29 +17,45 @@ export function ForgotPasswordForm() {
 
     const onSubmit = (data: ForgotPasswordFormData) => {
         console.log(data);
+
+        setRecoveyPassEmailSent(true);
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-                type="email"
-                placeholder="Email"
-                {...register("email", {
-                    required: "Введите email",
-                    pattern: {
-                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                        message: "Введите корректный email",
-                    },
-                })}
-            />
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    {...register("email", {
+                        required: "Введите email",
+                        pattern: {
+                            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                            message: "Введите корректный email",
+                        },
+                    })}
+                />
 
-            {errors.email && (
-                <span>{errors.email.message}</span>
-            )}
+                {errors.email && (
+                    <span>{errors.email.message}</span>
+                )}
 
-            <button type="submit">
-                Отправить
-            </button>
-        </form>
+                <button type="submit">
+                    Отправить
+                </button>
+
+            </form>
+            {
+                isRecoveyPassEmailSent && (
+                    <>
+                        <h2>Письмо отправлено</h2>
+
+                        <p>
+                            Проверьте вашу почту. Мы отправили письмо
+                            для восстановления пароля.
+                        </p>
+                    </>
+                )}
+        </>
     );
 }
