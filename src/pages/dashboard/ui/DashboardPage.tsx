88fa89bdc3a6a9e46a2d295ade5a0c758dd67bc4
Лёша <134238215+AlexKhabibov@users.noreply.github.com>
2026-08-29 18:04:00 +0300

    dashboard dev start

--- src/pages/dashboard/ui/DashboardPage.tsx
+++ src/pages/dashboard/ui/DashboardPage.tsx
@@ -1,22 +1,32 @@
 import { useGetProfileQuery } from "@/features/profile";
+import { Footer } from "@/widgets/footer";
+import { Sidebar } from "@/widgets/sidebar";
 
 export function DashboardPage() {
     const { data, isLoading, error } = useGetProfileQuery();
 
-    if (isLoading) {
-        return <h1>Загрузка...</h1>;
-    }
-
-    if (error) {
-        return <h1>Ошибка загрузки профиля</h1>;
-    }
-
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="dashboard">
 
-            <p>{data?.username}</p>
-            <p>{data?.email}</p>
+
+            <Sidebar />
+
+            <main className="dashboard__main">
+                {isLoading && <h1>Загрузка...</h1>}
+
+                {error && <h1>Ошибка загрузки профиля</h1>}
+
+                {data && (
+                    <>
+                        <h1>Dashboard</h1>
+
+                        <p>{data.username}</p>
+                        <p>{data.email}</p>
+                    </>
+                )}
+            </main>
+
+            <Footer />
         </div>
     );
 }
