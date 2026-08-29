3b0ad58d93b049d28b29a9b1fc6331c40f3e07df
Лёша <134238215+AlexKhabibov@users.noreply.github.com>
2026-08-29 12:58:14 +0300

    logout feature fix

--- src/features/logout/ui/LogoutButton.tsx
+++ src/features/logout/ui/LogoutButton.tsx
@@ -9,11 +9,11 @@ export function LogoutButton() {
     const handleLogout = async () => {
         try {
             await logout().unwrap();
-
-            removeAccessToken();
-            navigate("/auth/login", { replace: true });
         } catch (error) {
             console.log(error);
+        } finally {
+            removeAccessToken();
+            navigate("/auth/login", { replace: true });
         }
     };
 
